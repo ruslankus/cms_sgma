b9a3d7ddb20bebb2e6db1b2b614523afd15f1748
Alex V <darkoffalex@yandex.ru>
2015-03-23 17:47:44 +0200

    Working on menu management controller. Changed config file (Url manager class added)

--- protected/modules/admin/controllers/MenuController.php
+++ protected/modules/admin/controllers/MenuController.php
@@ -3,12 +3,59 @@
 class MenuController extends ControllerAdmin
 {
     /**
-     * Index
+     * Index - list all menu
      */
     public function actionIndex()
     {
         /* @var $menus ExtMenu[] */
         $menus = ExtMenu::model()->findAll();
         $this->render('index',array('menus' => $menus));
+    }
+
+
+    /**
+     * List  all items and other settings of menu
+     * @param $id
+     * @param int $page
+     * @throws CHttpException
+     */
+    public function actionEditMenu($id,$page = 1)
+    {
+        //find menu
+        $menu = ExtMenu::model()->findByPk((int)$id);
+
+        //if noy found
+        if(empty($menu))
+        {
+            throw new CHttpException(404);
+        }
+
+        //get all items
+        $items = $menu->getAllItemsSQL();
+
+        //pager stuff
+        $perPage = 10;
+        $total_pages = (int)ceil(count($items)/$perPage);
+        $offset = (int)($perPage * ($page - 1));
+        $itemsOfPage = array_slice($items,$offset,$perPage);
+
+        //currently selected theme
+        $selectedTheme = 'dark'; //TODO: select theme from DB
+
+        //get all templates for menus
+        $themeManager = Yii::app()->themeManager;
+        $dir = $themeManager->basePath.DS.$selectedTheme.DS.'views'.DS.'menus';
+        $files = scandir($dir);
+        $templates = array();
+
+        foreach($files as $fileName)
+        {
+            if($fileName != ".." && $fileName != ".")
+            {
+                $templates[] = $fileName;
+            }
+        }
+
+        $this->render('edit_menu',array('items' => $itemsOfPage, 'pages' => $total_pages, 'templates' => $templates));
     }
 }
---
+++ protected/modules/admin/views/menu/edit_menu.php
@@ -0,0 +1,7 @@
+<?php /* @var $items array() */ ?>
+<?php /* @var $pages int  */ ?>
+<?php /* @var $templates array() */ ?>
+
+<?php Debug::out($items); ?>
+<?php Debug::out($templates); ?>
+<?php Debug::out($pages); ?>
