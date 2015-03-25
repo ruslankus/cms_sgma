cee9fe1a5dcdc7ffe1c9eb9791d17fcbca8766c7
ruslankus <ruslankus@yahoo.com>
2015-03-25 19:00:04 +0200

    small fix

--- protected/modules/admin/controllers/SettingsController.php
+++ protected/modules/admin/controllers/SettingsController.php
@@ -15,6 +15,10 @@ class SettingsController extends ControllerAdmin
     
     public function actionEdit(){
         
+        if(!empty($_POST)){
+            Debug::d($_POST);
+        }
+        
         //$objSet = Settings::model()->findByAttributes(array('value_name' => 'setting2'));
        
         //$objSet->setting = "changed_value";
--- protected/modules/admin/views/settings/edit.php
+++ protected/modules/admin/views/settings/edit.php
@@ -1,7 +1,7 @@
 <?php $form=$this->beginWidget('CActiveForm'); ?>
     
     <?php foreach($arrData as $key => $value ): ?>
-<p><label><?php echo $key?></label><input type="text" name="<?php echo $key ?>" value="<?php echo $value?>" /></p>
+<p><label><?php echo $key?></label><input type="text" name="<?php echo $key ?>" value="<?php echo $value?>" /> <button type="submit" name="sub_<?echo $key?>">"Save_<?php echo $key?></button></p>
 
 <?php endforeach; ?>
 
