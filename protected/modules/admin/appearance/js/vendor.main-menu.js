aa9884c233d9ce5be19ce7f2022d6183bd100919
Alex V <darkoffalex@yandex.ru>
2015-03-26 15:12:52 +0200

    Pup-ups for deleting menu items

--- protected/modules/admin/appearance/js/vendor.main-menu.js
+++ protected/modules/admin/appearance/js/vendor.main-menu.js
@@ -5,49 +5,47 @@ $(document).on("change","#swapped-ids",function(){
 
     //TODO: Ajax reloading of all table
 	console.log("swapped "+this.value);
-
 	return false;
 });
 
 // Delete menu label event
 $(document).on("click", ".delete", function()
 	{
-		var data_id = $(this).parent().parent().attr("data-id");
-		var link = "_handles/popup-confirm.html#"+data_id;
-		$.popup({"url":link});
+        //e.preventDefault();
+		var popup_link = $(this).data('popup');
+        var href = $(this).attr('href');
+
+		$.popup({"url":popup_link});
 		$.popup.show();
 		return false;
 	});
 
-// Delete menu label event
-$(document).on("click", ".edit", function()
-	{
-		var data_id = $(this).parent().parent().attr("data-id");
-		$.popup("edit "+data_id+" ?");
-		$.popup.show();
-		return false;
-	});
 
 // move label up
 $(document).on("click", ".go-up", function()
 	{
-		var data_id = $(this).parent().parent().attr("data-id");
-		console.log(data_id+" up");
-		$(".sortable").load("_handles/main-menu.html", function() { $.enable_handlers(); });
-		return false;
+        var href = $(this).attr('href');
+        //TODO: Ajax reloading of all table
+        alert(href);
+        return false;
 	});
 
 $(document).on("click", ".go-down", function()
 	{
-		var data_id = $(this).parent().parent().attr("data-id");
-		console.log(data_id+" down");
-		$(".sortable").load("_handles/main-menu.html", function() { $.enable_handlers(); });
-		return false;
+        var href = $(this).attr('href');
+        //TODO: Ajax reloading of all table
+        alert(href);
+        return false;
 	});
 
 // for popup confirm
 $(document).on("click", ".unique-class-name", function(){
-	console.log("ok");
+
+    var href = $(this).attr('href');
+    //TODO: Ajax reloading of all table
+    alert(href);
+    $.popup.hide();
+
 	return false;
 });
 
