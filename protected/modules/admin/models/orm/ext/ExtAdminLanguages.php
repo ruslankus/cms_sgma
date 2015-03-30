36f170e4c7ccf9a94a7076682be3fb9e69ce7871
Maksim Jemeljanov <maksim.jemeljanov@gmail.com>
2015-03-30 10:34:19 +0300

    admin panel languages admin transaction fix

--- protected/modules/admin/models/orm/ext/ExtAdminLanguages.php
+++ protected/modules/admin/models/orm/ext/ExtAdminLanguages.php
@@ -37,61 +37,76 @@ Class ExtAdminLanguages extends AdminLanguages
     public function writeTrl($new_lang_name,$new_lang_prefix)
     {
         $con = $this->dbConnection;
-
-        $model = new AdminLanguages();
-        $model->name =  $new_lang_name;
-        $model->prefix =  $new_lang_prefix;
         
-        $save = $model->save();
-        $lang_id=$model->id;
-        $arrLabels = AdminLabels::model()->findAll();
-        $arrMessages = ExtAdminMessages::model()->findAll();
+        $transaction = $con->beginTransaction();
+        try{
+        
+            /*
+            $model = new AdminLanguages();
+            $model->name =  $new_lang_name;
+            $model->prefix =  $new_lang_prefix;
+            $save = $model->save();
+            $lang_id=$model->id;
+            */
 
-        foreach($arrLabels as $label):
-            $label_id = $label->id;
-            $sql_labels  = "INSERT INTO labels_trl (`lng_id`, `translation_id`) VALUES ({$lang_id}, {$label_id})";
+            $sql_lang = "INSERT INTO languages (`name`, `prefix`) VALUES ('{$new_lang_name}', '{$new_lang_prefix}')";
+            $con->createCommand($sql_lang)->execute();
+            $lang_id = $con->getLastInsertID('languages');
+
+            $arrLabels = AdminLabels::model()->findAll();
+            $arrMessages = ExtAdminMessages::model()->findAll();
+
+            foreach($arrLabels as $label):
+                $label_id = $label->id;
+                $sql_labels  = "INSERT INTO labels_trl (`lng_id`, `translation_id`) VALUES ({$lang_id}, {$label_id})";
+                $con->createCommand($sql_labels)->execute();
+            endforeach;
+
+            /*
+            $sql_labels  = "INSERT INTO labels_trl (`lng_id`, `translation_id`) ";
+            $sql_labels .= "VALUES ";       
+            $i=0;
+            foreach($arrLabels as $label):
+                //$label_id .= "id=".$label->id." | label".$label->label."<br>";
+                $label_id = $label->id;
+                if($i == 0){
+                    $sql_labels .= "({$lang_id}, {$label_id}) ";
+                }else{
+                     $sql_labels .= ",({$lang_id}, {$label_id}) ";
+                } 
+                $i++;
+            endforeach;
             $con->createCommand($sql_labels)->execute();
-        endforeach;
+            */
 
-        /*
-        $sql_labels  = "INSERT INTO labels_trl (`lng_id`, `translation_id`) ";
-        $sql_labels .= "VALUES ";       
-        $i=0;
-        foreach($arrLabels as $label):
-            //$label_id .= "id=".$label->id." | label".$label->label."<br>";
-            $label_id = $label->id;
-            if($i == 0){
-                $sql_labels .= "({$lang_id}, {$label_id}) ";
-            }else{
-                 $sql_labels .= ",({$lang_id}, {$label_id}) ";
-            } 
-            $i++;
-        endforeach;
-        $con->createCommand($sql_labels)->execute();
-        */
+            foreach($arrMessages as $message):
+                $message_id = $message->id;
+                $sql_messages  = "INSERT INTO messages_trl (`lng_id`, `translation_id`) VALUES ({$lang_id}, {$message_id})";
+                $con->createCommand($sql_messages)->execute();
+            endforeach;
 
-        foreach($arrMessages as $message):
-            $message_id = $message->id;
-            $sql_messages  = "INSERT INTO messages_trl (`lng_id`, `translation_id`) VALUES ({$lang_id}, {$message_id})";
+            /*
+            $sql_messages  = "INSERT INTO messages_trl (`lng_id`, `translation_id`) ";
+            $sql_messages .= "VALUES ";       
+            $i=0;
+            foreach($arrMessages as $message):
+                $message_id = $message->id;
+                if($i == 0){
+                    $sql_messages .= "({$lang_id}, {$message_id}) ";
+                }else{
+                     $sql_messages .= ",({$lang_id}, {$message_id}) ";
+                } 
+                $i++;
+            endforeach;
             $con->createCommand($sql_messages)->execute();
-        endforeach;
-
-        /*
-        $sql_messages  = "INSERT INTO messages_trl (`lng_id`, `translation_id`) ";
-        $sql_messages .= "VALUES ";       
-        $i=0;
-        foreach($arrMessages as $message):
-            $message_id = $message->id;
-            if($i == 0){
-                $sql_messages .= "({$lang_id}, {$message_id}) ";
-            }else{
-                 $sql_messages .= ",({$lang_id}, {$message_id}) ";
-            } 
-            $i++;
-        endforeach;
-        $con->createCommand($sql_messages)->execute();
-        */
-
-        return true;
+            */
+        
+            $transaction->commit();
+            return true; 
+        }catch(Exception $e){
+            $transaction->rollback();
+            return false;
+        }
+        
     }
 }    
