054336cdd28f9f16436d1753b8da91c79be800fb
Alex V <darkoffalex@yandex.ru>
2015-04-01 16:40:02 +0300

    Outputting registered menus to the layout - done

--- themes/dark/views/menus/other_menu.php
+++ themes/dark/views/menus/other_menu.php
@@ -2,9 +2,9 @@
 <?php /* @var $items_inline Array */ ?>
 <?php /* @var $items_nested Array */ ?>
 
-<ul>
-    <?php foreach($items_inline as $item): ?>
-        <li <?php if($item['active']): ?> class="active" <?php endif; ?> style="text-indent: <?php echo ($item['nesting_level']-1)*10; ?>px">
+<ul style="list-style: none">
+    <?php foreach($items_nested as $item): ?>
+        <li <?php if($item['active']): ?> class="active" <?php endif; ?> style="display: block; float: left; padding: 5px; font-weight: bold;">
             <a href="<?php echo $item['url'] ?>"><?php echo $item['name'] ?></a>
         </li>
     <?php endforeach; ?>
