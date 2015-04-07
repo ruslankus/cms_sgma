63c2ad8db2b712a21ad46950122f7298e3453dbc
Maksim Jemeljanov <maksim.jemeljanov@gmail.com>
2015-04-07 15:09:56 +0300

    add smtp settings to Mailer helper (commented)

--- protected/helpers/Mailer.php
+++ protected/helpers/Mailer.php
@@ -3,12 +3,24 @@ Yii::import('application.vendor.*');
 require_once('phpmailer/class.phpmailer.php');
 
 class Mailer {
+/*
+	example of params
+	array('name'=>'Cms user', 'mail'=>'user@test.com','subject' => 'Test Mail','body' => 'mail text entered by user in textarea');
+	link for test: site.root/maxim
 
+*/
 	public static function sendMail($params)
 	{
 		
 		$mail = new PHPMailer;
+/*
+		$mail->SMTPAuth   = true;                  // enable SMTP authentication
+		$mail->Host       = "mail.yourdomain.com"; // sets the SMTP server
+		$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
+		$mail->Username   = "yourname@yourdomain"; // SMTP account username
+		$mail->Password   = "yourpassword";        // SMTP account password
 
+*/
 		//$mail->AddReplyTo($to,"Reply toto");
 		 
 		$mail->SetFrom('cms@cms.com', 'ContactForm');
