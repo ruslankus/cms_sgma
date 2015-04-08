6dcedd14a2f838744fd20fb7a6742989d4e5f5f9
ruslankus <ruslankus@yahoo.com>
2015-04-08 14:03:27 +0300

    added active language attribute

--- protected/helpers/SiteLng.php
+++ protected/helpers/SiteLng.php
@@ -21,7 +21,7 @@ class SiteLng
     
      private function __construct() {
 
-        $this->_objLngs = Languages::model()->findAll();
+        $this->_objLngs = Languages::model()->findAllByAttributes(array('active' => 1));
         $prefix = Yii::app()->language;
         foreach($this->_objLngs as $lng){
             if($lng->prefix == $prefix){
