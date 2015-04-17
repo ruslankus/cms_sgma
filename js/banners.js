5e280fea0ba965cdaeb2fd9b95d5c4143d41b6f1
Maksim Jemeljanov <maksim.jemeljanov@gmail.com>
2015-04-17 11:44:46 +0300

    slider output done

--- js/banners.js
+++ js/banners.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
        navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
-      singleItem:true
- 
+      singleItem:true,
+      navigation:false
       // "singleItem:true" is a shortcut for:
       // items : 1, 
       // itemsDesktop : false,
