503c228e9224dff1aa343c56cd6d1c8ae9804022
Maksim Jemeljanov <maksim.jemeljanov@gmail.com>
2015-04-29 11:35:36 +0300

    contact form widget fix

---
+++ js/send-form-widget.js
@@ -0,0 +1,20 @@
+$(document).ready(function(e) {
+    $('.send-data').click(function(){
+    	var prefix = $('#lang_prefix').val();
+    	var email = $('#email').val();
+    	var text = $('#text').val();
+    	var code = $('#code').val();
+        var name = $('#name').val();
+        var link = '/'+ prefix +'/mail/AjaxMailWidgetCheck';
+        $.ajaxSetup({async:false});
+        $.ajax({ type: "post",url:link,data:{email:email,text:text,code:code,name:name}}).done(function(data){
+            
+            obj = jQuery.parseJSON(data);
+            console.log(obj);
+            $('.form-result').html(obj.result);
+            $('#code').val('');
+            $("#yw0_button").click();
+        });
+        return false;     
+    });
+});
